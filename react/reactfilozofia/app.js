bf14f22acfe60e427e30e91b34f3a0d60671718d
GáborBence <gabor.bence2005@gmail.com>
2024-12-09 14:52:37 +0100

    2024.12.09
    nativ gomb

---
+++ react/reactfilozofia/app.js
@@ -0,0 +1,11 @@
+const btn = document.createElement('button');
+
+btn.onclick=function()
+{    
+    alert("Ez egy nativ kód");
+}
+
+btn.innerHTML="Natív gomb";
+
+document.getElementById("nativ-button-container").appendChild(btn);
+ 
