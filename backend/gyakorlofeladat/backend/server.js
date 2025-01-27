36e597f1f2f07398039797524b06648e3abab433
GáborBence <gabben878@hengersor.hu>
2025-01-27 13:25:36 +0100

    server

---
+++ backend/gyakorlofeladat/backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require ("mysql");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
+ 
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    password: "",
+    database:"kozutak",
+})
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+})
+ 
+app.get("/regiok",(req, res) => {
+    const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+ 
+app.listen(3001, () => {
+    console.log("Server is runningg on port 3001");
+});
